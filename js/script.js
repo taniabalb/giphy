9ec586518d1f0cb10a7250c17f7a904aa181d5d0
Tania Balbuena <tbalbuenaa@gmail.com>
2019-04-03 21:55:18 +0000

    working with matt who is the best

--- js/script.js
+++ js/script.js
@@ -4,8 +4,36 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+  var search = $("#search-term").val();
+  console.log(search);
+  var api = "https://api.giphy.com/v1/gifs/search?q="+search+ "&rating=pg&api_key=dc6zaTOxFJmzC";
+  $.ajax({
+      url : api,
+      method: "GET",
+      success: function(response){
+          $(".gallery").html("");
+         
+          response.data.forEach(function(element) {
+            var results = '<img src="' + element.images.original.url+ '"/>';
+            $(".gallery").append(results);
+          });
+        
+         
+      }
+    
+  });
+
+   $(document).keypress(function(event){
+        console.log(event);
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+        console.log(response); 
+        }
+    });
 });
+
+
+
+
+
 
